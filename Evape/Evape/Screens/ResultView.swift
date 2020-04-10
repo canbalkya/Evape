fe12c7e14bae4989a5e92c67d7c7230847ff2f18
Can Balkaya <canbalkya@gmail.com>
2020-04-10 19:15:20 +0300

    ResultView is created.

---
+++ Evape/Evape/Screens/ResultView.swift
@@ -0,0 +1,21 @@
+//
+//  ResultView.swift
+//  Evape
+//
+//  Created by Can Balkaya on 4/10/20.
+//  Copyright © 2020 Can Balkaya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ResultView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResultView()
+    }
+}
